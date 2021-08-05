51100c5e8f0a631658de53187c977e94940ed9e1
Pasha <pashahrytsyna@gmail.com>
2021-08-05 11:54:30 +0300

    first commit

---
+++ task-01.js
@@ -0,0 +1,21 @@
+function Account(obj) {
+  this.login = obj.login;
+  this.email = obj.email;
+}
+
+Account.prototype.getInfo = function () {
+  console.log(`Login: ${this.login}, Email: ${this.email}`);
+};
+
+const mango = new Account({
+  login: "Mangozedog",
+  email: "mango@dog.woof",
+});
+
+console.log(mango);
+mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+const poly = new Account({
+  login: "Poly",
+  email: "poly@mail.com",
+});
+poly.getInfo(); // Login: Poly, Email: poly@mail.com
---
+++ task-03.js
@@ -0,0 +1,33 @@
+class Storage {
+  constructor(arr) {
+    this.items = arr;
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(item) {
+    this.items.push(item);
+  }
+
+  removeItem(item) {
+    if (this.items.includes(item)) {
+      let index = this.items.indexOf(item);
+      this.items.splice(index, 1);
+    }
+  }
+}
+
+const storage = new Storage([
+  "Нанитоиды",
+  "Пролонгер",
+  "Железные жупи",
+  "Антигравитатор",
+]);
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+storage.addItem("Дроид");
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+storage.removeItem("Пролонгер");
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
